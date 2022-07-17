f1a99207f8ccacfc2429f6504a2a79511d19135d
OAaronPeter <ocenaaronpeter2001@gmail.com>
2022-07-17 11:28:17 +0300

    first commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,25 +1,26 @@
-// const path = require(`path`)
+const path = require(`path`)
 
-// exports.createPages = async ({ graphql, actions }) => {
+exports.createPages = async ({ graphql, actions }) => {
 
-//   const { data } = await graphql(`
-//     query Articles {
-//       allMarkdownRemark {
-//         nodes {
-//           frontmatter {
-//             slug
-//           }
-//         }
-//       }
-//     }
-//   `)
+  const { data } = await graphql(`
+    query Articles {
+      allMarkdownRemark {
+        nodes {
+          frontmatter {
+            slug
+          }
+        }
+      }
+    }
+  `)
 
-//   data.allMarkdownRemark.nodes.forEach(node => {
-//     actions.createPage({
-//       path: '/projects/'+ node.frontmatter.slug,
-//       component: path.resolve('./src/templates/project-details.js'),
-//       context: { slug: node.frontmatter.slug }
-//     })
-//   })
+  data.allMarkdownRemark.nodes.forEach(node => {
+    actions.createPage({
+      path: '/projects/'+ node.frontmatter.slug,
+      component: path.resolve('./src/templates/project-details.js'),
+      context: { slug: node.frontmatter.slug }
+    })
+  })
 
-// }
+}
+
