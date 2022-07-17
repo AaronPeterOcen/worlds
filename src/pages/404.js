a0588eab4e9d836c64fbd2c95d0b8e7fc739c2fd
Aaronpteer <aaronpteer@pop-os.localdomain>
2022-07-17 10:27:45 +0300

    first commit

---
+++ src/pages/404.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Layout from '../comps/Layout'
+
+export default function NotFound() {
+  return (
+  <Layout> 
+    <div>
+      <h2>
+      404
+      </h2>
+      <p>Page not found</p>
+  </div>
+  </Layout>
+   
+  )
+}
---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../comps/Layout'
+
+export default function  about() {
+  return (
+      <Layout>
+              <div>about</div>
+
+      </Layout>
+  )
+}
